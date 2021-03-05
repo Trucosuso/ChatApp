3ca51ad5f3524dbabb3e5164d61ccce44d6661d4
Jesús L. Pinzolas <jesus@trucosuso.com>
2021-03-05 22:20:25 +0100

    Added option to send bold text

--- public/javascripts/chatApp.js
+++ public/javascripts/chatApp.js
@@ -4,6 +4,9 @@ var socket = io();
 /** @type {String} Name of the user */
 var username = "";
 
+/** @type {Boolean} True if text is bold */
+var textBold = false;
+
 /* Store DOM elements to work with them */
 var chat = document.getElementById("chat");
 var login = document.getElementById("login");
@@ -12,6 +15,31 @@ var loginInput = document.getElementById("loginInput");
 var messages = document.getElementById("messages");
 var chatForm = document.getElementById("form");
 var chatInput = document.getElementById("input");
+
+/* Do not press boldButton. Press send sendButton instead */
+chatInput.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        e.preventDefault();
+        document.getElementById("sendButton").click();
+    }
+});
+
+/* Change textBold when boldButton is pressed */
+var boldButton = document.getElementById("boldButton");
+boldButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    textBold = !textBold;
+    if (textBold) {
+        boldButton.style.color = "#000";
+        boldButton.style.backgroundColor = "#999";
+        chatInput.style.fontWeight = "bold";
+    } else {
+        boldButton.style.color = "#fff";
+        boldButton.style.backgroundColor = "#333";
+        chatInput.style.fontWeight = "normal";
+    }
+});
+
 
 /** Event to store username and change view to chat */
 loginForm.addEventListener("submit", function (e) {
@@ -32,6 +60,7 @@ chatForm.addEventListener("submit", function (e) {
     if (chatInput.value) {
         socket.emit("chat message", {
             user: username,
+            bold: textBold,
             message: chatInput.value
         });
         chatInput.value = "";
@@ -43,7 +72,13 @@ socket.on("chat message", function (msg) {
     let item = document.createElement("li");
     let messageUsername = document.createElement("strong");
     messageUsername.textContent = msg.user + ": ";
-    let messageText = document.createElement("span");
+    let messageText = null;
+    if (msg.bold) {
+        messageText = document.createElement("strong");
+    } else {
+        messageText = document.createElement("span");
+    }
+    
     messageText.textContent = msg.message;
 
     item.append(messageUsername, messageText);
